d7d52a03a685476df3ddb107e835557a05546e49
wemendez2801 <wemendez.20@est.ucab.edu.ve>
2024-12-02 13:36:11 -0400

    update
    cambios

--- interfaz.py
+++ interfaz.py
@@ -1,7 +1,8 @@
 import customtkinter
+import tkinter as tk
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("green")
 
 root = customtkinter.CTk()
 root.geometry("500x350")
@@ -17,13 +18,13 @@ frame.pack(pady=20, padx=20, fill="both", expand=True)
 label = customtkinter.CTkLabel(master=frame, text="Traductor de Lenguaje de Señas Venezolano Educativo")
 label.pack(pady=12 , padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Iniciar")
+button = customtkinter.CTkButton(master=frame, fg_color="blue", text="Iniciar")
 button.pack(pady=12,padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Sobre la aplicación")
+button = customtkinter.CTkButton(master=frame, fg_color="blue", text="Sobre la aplicación")
 button.pack(pady=12,padx=10)
 
-button = customtkinter.CTkButton(master=frame, text="Salir")
+button = customtkinter.CTkButton(master=frame, fg_color="red", text="Salir")
 button.pack(pady=12,padx=10)
 
 root.mainloop()

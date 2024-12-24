460af34f99047b2c025da949e8c90de25456a12e
wemendez2801 <wemendez.20@est.ucab.edu.ve>
2024-12-24 13:47:16 -0400

    arreglos
    arreglos

--- imagenes.py
+++ imagenes.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 
-#Creacion de carpeta
+# Creación de carpeta
 DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
@@ -23,17 +23,21 @@ def initialize_camera(camera_index=0):
 # Inicializar la cámara
 cap = initialize_camera()
 
-#Coleccion de imágenes
+# Colección de imágenes
 for j in range(number_of_classes):
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
 
-    print('Collecting data for class {}'.format(j))
+    print('Recolectando datos para la clase: {}'.format(j))
 
-    done = False
     while True:
         ret, frame = cap.read()
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+        if not ret:
+            print("Error al capturar el frame.")
+            break
+
+        frame = cv2.flip(frame, 1)  # Invertir la imagen horizontalmente
+        cv2.putText(frame, 'Presiona Q para empezar :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
                     cv2.LINE_AA)
         cv2.imshow('frame', frame)
         if cv2.waitKey(25) == ord('q'):
@@ -42,6 +46,11 @@ for j in range(number_of_classes):
     counter = 0
     while counter < dataset_size:
         ret, frame = cap.read()
+        if not ret:
+            print("Error al capturar el frame.")
+            break
+
+        frame = cv2.flip(frame, 1)  # Invertir la imagen horizontalmente
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
         cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
